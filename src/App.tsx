6445c7a1a7cc01c9c38582236b74649cf07c954a
naetharu <naetharu@gmail.com>
2024-07-29 13:59:24 +0100

    Add background color and image to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import styles from "./App.module.css";
+import mbBgLight from "./assets/pattern-background-mobile-light.svg";
+
 function App() {
-  return <div>Clean app ready to start</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.bgImageContainer}>
+        <img src={mbBgLight} alt="background pattern" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
