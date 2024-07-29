0f1dd36dd61b2eb40ed9308b3800ccbfca1147fc
naetharu <naetharu@gmail.com>
2024-07-29 14:11:29 +0100

    Switch image to be background image in div

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import mbBgLight from "./assets/pattern-background-mobile-light.svg";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className={styles.container}>
-      <div className={styles.bgImageContainer}>
+      {/* <div className={styles.bgImageContainer}>
         <img src={mbBgLight} alt="background pattern" />
-      </div>
+      </div> */}
+      <Header />
     </div>
   );
 }
