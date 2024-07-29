247a3e1c89f5da66b1bd1bd75e7737f164301129
naetharu <naetharu@gmail.com>
2024-07-29 18:48:33 +0100

    Add darkmode styles to app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import styles from "./App.module.css";
 import Header from "./components/Header/Header";
+import { useTheme } from "./contexts/ThemeContext";
 
-function App() {
+const App = () => {
+  const { darkMode } = useTheme();
+
+  useEffect(() => {
+    console.log(darkMode);
+
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   return (
     <div className={styles.container}>
       <Header />
     </div>
   );
-}
+};
 
 export default App;
